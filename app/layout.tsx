6456b797919e7e295162228347c323affbbd1ac8
mysleekdesigns <97188303+sls-slacey@users.noreply.github.com>
2025-06-19 18:24:23 -0400

    improved form everything is working well now

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,33 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/Navbar'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'AI Bio Generator | Brought to you by Swing.com',
-  description: 'Nextjs AI Bio Generator',
-}
+  title: "AI Bio Generator and Image Analyzer | Brought to you by Swing.com",
+  description: "Nextjs AI Bio Generator and Image Analyzer",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className='flex flex-col min-h-screen'>
+    <html lang="en">
+      <body className={`${inter.variable} antialiased font-inter`}>
+        <div className="flex flex-col min-h-screen">
           {/* Navbar can be added here if needed */}
           <Navbar />
-          <main className='flex-1'>{children}</main>
+          <main className="flex-1">{children}</main>
           {/* Footer can be added here if needed */}
         </div>
       </body>
     </html>
-  )
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,22 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between p-4 bg-gray-800 text-white'>
-      <ul className='flex items-center space-x-4'>
+    <div className="flex items-center justify-between p-4 bg-gray-900 text-white">
+      <ul className="flex items-center space-x-4">
         <li>
-          <Link href='/' className='text-md font-semibold'>
+          <Link href="/" className="text-md font-semibold">
             AI Bio Autocomplete
           </Link>
         </li>
-        <li className='ml-6'>
-          <Link href='/ai-image' className='text-md font-semibold'>
+        <li className="ml-6">
+          <Link href="/ai-image" className="text-md font-semibold">
             AI Image Analysis
           </Link>
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
